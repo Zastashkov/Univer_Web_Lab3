50bfe06ed375f0b1bb49ce87c1b5d1ae3c83fd26
daniil <zast2020@mail.ru>
2023-01-11 17:55:30 +0800

    sec

--- index.php
+++ index.php
@@ -1,1 +1,80 @@
-<?php
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>211-361 Засташков ЛР 2</title>
+    <link rel="stylesheet" href="main.css">
+    <?php
+    $name = "Засташков Даниил 211-361 Лаб_3";
+    echo '<title>Засташков Даниил 211-361 Лаб_3</title>'
+    ?>
+</head>
+<body>
+<header class="header">
+    <div class="container">
+        <nav class="main-menu">
+            <a href="#main">Моя страница</a>
+            <a href="#about">Обо мне</a>
+            <a href="#skills">Навыки</a>
+            <a href="#technologies">Технологии</a>
+            <a href="feedback.html">Обратная связь</a>
+            <a href="registration.html">Регистрация</a>
+        </nav>
+        <div class="text-center py-5" >
+            <h1 class="name">Засташков Даниил Андреевич</h1>
+            <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+            <h3>Разработчик мобильных приложений</h3>
+            <a class="btn contact-me-btn" href="mailto:zast2020@mail.ru">Написать мне</a>
+        </div>
+    </div>
+</header>
+
+<main>
+    <div class="container">
+        <section id="about">
+            <h1>Обо мне</h1>
+            <figure class="avatar">
+                <img src="images/person.svg" alt="Person">
+            </figure>
+            <p>
+                Кстати, активно развивающиеся страны третьего мира будут призваны к ответу. Картельные сговоры не допускают ситуации, при которой многие известные личности, превозмогая сложившуюся непростую экономическую ситуацию, объединены в целые кластеры себе подобных. Повседневная практика показывает, что дальнейшее развитие различных форм деятельности способствует повышению качества новых предложений. Но многие известные личности обнародованы.
+            </p>
+        </section>
+
+        <section id="skills">
+            <h1>Технологии</h1>
+            <p>
+                Следует отметить, что существующая теория способствует повышению качества экспериментов поражающих по своей масштабности и грандиозности. Приятно, граждане, наблюдать, как тщательные исследования конкурентов ограничены исключительно образом мышления. Внезапно, интерактивные прототипы призывают нас к новым свершениям, которые, в свою очередь, должны быть описаны максимально подробно.
+            </p>
+            <div class="text-right">
+                <a class="btn" href="skills.html">Подробнее</a>
+            </div>
+        </section>
+    </div>
+</main>
+
+<section id="key-skills" class="key-skills">
+    <h2>Ключевые навыки</h2>
+
+    <p class="table-skills">
+    <table>
+        <tr>
+            <th>C</th>
+            <th>Java</th>
+            <th>Linux</th>
+            <th>SQL</th>
+            <th>Bash</th>
+        </tr>
+    </table>
+    </p>
+</section>
+
+<footer class="site-footer">
+    <div class="container">
+        <p>Email: zast2020@mail.ru</p>
+        <p>Phone: +7(923)082-22-72</p>
+        <p>&copy; Засташков Д. А.</p>
+    </div>
+</footer>
+</body>
+</html>
